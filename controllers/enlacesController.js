e7d5f015f03ce52e45a0547fcfc31a5be1b4647c
Pedro Feliu <pedro@medanosol.es>
2020-10-26 12:50:29 +0000

    26102020

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -11,11 +11,11 @@ exports.nuevoEnlace = async (req, res, next) => {
   }
 
   //Save in DDBB
-  const { nombre_original } = req.body;
+  const { nombre_original, nombre } = req.body;
 
   const enlace = new Enlaces();
   enlace.url = shortid.generate();
-  enlace.nombre = shortid.generate();
+  enlace.nombre = nombre;
   enlace.nombre_original = nombre_original;
 
   //if user is auth
